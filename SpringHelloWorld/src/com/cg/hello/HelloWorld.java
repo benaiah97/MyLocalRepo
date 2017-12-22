8e88fd9102d8d03fb3107a31ba5826835293ef63
ben <kbenaiah@gmail.com>
2017-12-22 16:12:22 +0530

    Project
    Spring Hello World

---
+++ SpringHelloWorld/src/com/cg/hello/HelloWorld.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.cg.hello;
+
+/**
+ * @author benaim
+ *
+ */
+public class HelloWorld {
+
+	private String message;
+
+	public HelloWorld() {
+
+	}
+
+	public HelloWorld(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorld [message=" + message + "]";
+	}
+
+}
